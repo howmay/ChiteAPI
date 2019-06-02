1bc0d64a5a5cbd8fce636350f881342e1882a725
harvey.chen <harvey.chen.0425@gmail.com>
2019-06-02 16:45:31 +0800

    add orm path

--- chite/product/api.py
+++ chite/product/api.py
@@ -2,25 +2,25 @@
 
 from . import Product
 from flask import jsonify, request, session, logging, abort
-from chite.store import MongoDB
 from bson import json_util
+from chite.model import Product
 
 
 @Product.route('/product', methods=['GET'])
 def GetProduct():
-    return '', 200
+    return jsonify({}), 200
 
 
 @Product.route('/product', methods=['POST'])
 def PostProduct():
-    return '', 200
+    return jsonify({}), 200
 
 
 @Product.route('/product', methods=['PUT'])
 def PutProduct():
-    return '', 200
+    return jsonify({}), 200
 
 
 @Product.route('/product', methods=['DELETE'])
 def DeleteProduct():
-    return '', 200
+    return jsonify({}), 200
