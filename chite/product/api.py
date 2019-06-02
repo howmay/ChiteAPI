4579cdec67ef26decd84d3adb9f17827092e46d0
harvey.chen <harvey.chen.0425@gmail.com>
2019-06-03 00:05:07 +0800

    add requests example

--- chite/product/api.py
+++ chite/product/api.py
@@ -26,3 +26,21 @@ def PutProduct():
 @Product.route('/product', methods=['DELETE'])
 def DeleteProduct():
     return jsonify({}), 200
+
+
+# {
+#     "_id":"9001401801"
+# 	  "material": "MDF",
+#     "images": ["http://www.google.com/1.jpg"],
+#     "productName": "White-客供色版",
+#     "specificatio": ["800*480*15MM","800*115*15MM"],
+#     "unreportedTax": 85.0,
+#     "accessories_list": [{
+#     	"images": ["http://www.google.com/1.jpg"],
+# 	    "ai_id": "A",
+# 	    "accessoriesName": "平面內六角",
+# 	    "specification": ["M6*28MM 黑色","生產改為, M6*30MM"],
+# 	    "set_num": 6,
+# 	    "unit": "PCS"
+#     }]
+# }
