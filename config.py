6fcaa441a22b6dd70067501a98eed9ab1578df35
harvey.chen <harvey.chen.0425@gmail.com>
2019-06-02 16:44:36 +0800

    change config to orm config

--- config.py
+++ config.py
@@ -9,9 +9,10 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    MONGO_URI = os.getenv(
-        'MONGO_URL', 'mongodb://python-stock:bNMbTAddsZEwpVRI@amazondata-shard-00-00-7op9t.gcp.mongodb.net:27017,amazondata-shard-00-01-7op9t.gcp.mongodb.net:27017,amazondata-shard-00-02-7op9t.gcp.mongodb.net:27017/chite?ssl=true&replicaSet=AmazonData-shard-0&authSource=admin&retryWrites=true&w=majority'
-    )
+    MONGODB_SETTINGS = {
+        'db': 'chite',
+        'host': 'mongodb://python-stock:bNMbTAddsZEwpVRI@amazondata-shard-00-00-7op9t.gcp.mongodb.net:27017,amazondata-shard-00-01-7op9t.gcp.mongodb.net:27017,amazondata-shard-00-02-7op9t.gcp.mongodb.net:27017/chite?ssl=true&replicaSet=AmazonData-shard-0&authSource=admin&retryWrites=true&w=majority'
+    }
 
 
 class ProductionConfig(Config):
